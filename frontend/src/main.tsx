946e3f166703fe99571003d5627ed12477c8a2a8
Slade <saib6064@gmail.com>
2026-01-13 14:37:06 +0700

    feat(frontend): change def pag

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home.tsx";
+import App from "./App.tsx";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
